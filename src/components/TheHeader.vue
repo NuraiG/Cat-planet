b64c24075db5477601ddd25ed39a3e7e3787a2e8
Nuray Gulistan <nuray.b.gulistan@gmail.com>
2021-06-30 13:18:38 +0300

    Added header

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container-flex">
+    <h2>The Cats</h2>
+  </div>
+</template>
+<style scoped>
+.container-flex {
+  background-color: cornflowerblue;
+  display: flex;
+  justify-content: space-between;
+  height: 60px;
+}
+h2 {
+  color: rgb(223, 240, 237);
+  font-family: monospace;
+  padding: 3px;
+  margin: auto 2px ;
+}
+</style>
